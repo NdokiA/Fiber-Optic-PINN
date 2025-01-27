9ec66d1a237e0efc5da97abc259c47527f42f640
Ndiko-A <andikoputrapk@student.ub.ac.id>
2025-01-27 21:26:28 +0700

    Revise structure for propagation to adhere with gradient.py and initializer.py by adding input(T,L,regularizer_param. Separate the residue and labelled loss training

--- optimizer.py
+++ optimizer.py
@@ -9,18 +9,20 @@ class pinnOptimizer(nlseGradient):
     Algorithm for processing input and do backpropagation
     '''
     
-    def __init__(self, model, batched_dict, alpha, beta2, gamma, penalty = True):
+    def __init__(self, model, batched_dict ,T, L, alpha, beta2, gamma, penalty):
         super().__init__()
         self.model = model
         self.optimizer = tfp.optimizer.lbfgs_minimize
         
-        labelled_keys = [key for key in batched_dict.keys() if key != 'col_point']
+        self.labelled_data = batched_dict['labelled_data']
         self.collocation_data = batched_dict['col_point']
-        self.labelled_data = tf.data.Dataset.zip(tuple(batched_dict[key] for key in labelled_keys))
         
         self.alpha = alpha
         self.beta2 = beta2
         self.gamma = gamma 
+        
+        self.T = T 
+        self.L = L
 
         self.penalty = penalty
         
@@ -39,15 +41,18 @@ class pinnOptimizer(nlseGradient):
             tf.Tensor: scalar tensor of the total residue loss of real and imaginary part
         """        
         
-        u_residue, v_residue, correction = self.compute_residue(collocation_batch[0], self.model,
-                                self.gamma, self.beta2, self.alpha, self.penalty)
+        u_residue, v_residue, correction = self.compute_residue(
+                                                    collocation_batch[0], self.model,
+                                                    self.T, self.L,
+                                                    self.gamma, self.beta2, self.alpha, self.penalty
+                                                    )
         
         loss_u = tf.reduce_mean(tf.keras.losses.MSE(u_residue, collocation_batch[1]), axis = 0)
         loss_v = tf.reduce_mean(tf.keras.losses.MSE(v_residue, collocation_batch[2]), axis = 0)
         total_loss = tf.cast(loss_u + loss_v + correction, tf.float32)
         return total_loss
 
-    def _compute_labelled_loss(self, one_labelled_batch) -> tf.Tensor:
+    def _compute_labelled_loss(self, labelled_batch) -> tf.Tensor:
         """
         Compute labelled loss of a batch of labelled data
 
@@ -58,38 +63,32 @@ class pinnOptimizer(nlseGradient):
         Returns:
             tf.Tensor: scalar tensor of total loss of real and imaginary part of labelled data
         """        
+    
+        computed_u, computed_v = self.compute_labelled_data(labelled_batch[0], self.model)
         
-        #Tiga proses di bawah perlu diubah ke global untuk seluruh batch labelled data daripada lokal, kelamaan.
-        tx_label = tf.concat([batch[0] for batch in one_labelled_batch], axis = 0)
-        u_label = tf.concat([batch[1] for batch in one_labelled_batch], axis = 0)
-        v_label = tf.concat([batch[2] for batch in one_labelled_batch], axis = 0)
-        
-        computed_u, computed_v = self.compute_labelled_data(tx_label, self.model)
-        
-        loss_u = tf.reduce_mean(tf.keras.losses.MSE(computed_u, u_label))
-        loss_v = tf.reduce_mean(tf.keras.losses.MSE(computed_v, v_label))
+        loss_u = tf.reduce_mean(tf.keras.losses.MSE(computed_u, labelled_batch[1]))
+        loss_v = tf.reduce_mean(tf.keras.losses.MSE(computed_v, labelled_batch[2]))
         total_loss = tf.cast(loss_u + loss_v, tf.float32)
         return total_loss
 
 
-    def _compute_gradient(self, collocation_batch, one_labelled_batch) -> tuple:        
+    def _compute_gradient(self, batch, is_residue) -> tuple:        
         """
 
         Args:
-            collocation_batch (tuple(EagerTensor)): one batch of collocation point data
-            one_labelled_batch (tuple(EagerTensor)): one batch of labelled point data
+            batch: one batch of data
+            is_residue: check whether to use residue or labelled loss
 
         Returns:
-            current_residue_loss, current_labelled_loss, total_loss, flattened_gradients
+            loss, flattened gradients
         """        
+        func = self._compute_residue_loss if is_residue else self._compute_labelled_loss
         with tf.GradientTape() as tape:
-            current_residue_loss = self._compute_residue_loss(collocation_batch)
-            current_labelled_loss = self._compute_labelled_loss(one_labelled_batch)
-            total_loss = current_residue_loss+current_labelled_loss
+            loss = func(batch)
         
-        grads = tape.gradient(total_loss, self.model.trainable_variables)
+        grads = tape.gradient(loss, self.model.trainable_variables)
         flattened_gradients = tf.concat([tf.reshape(g, [-1]) for g in grads], axis=0)
-        return total_loss, flattened_gradients 
+        return loss, flattened_gradients 
     
     def assign_model_parameters(self, position):
         """
@@ -105,20 +104,19 @@ class pinnOptimizer(nlseGradient):
             var.assign(tf.reshape(opt_var, var.shape))
         
     
-    def optimize_single_batch(self, collocation_batch, labelled_batch) -> tuple[tf.Tensor]:
+    def optimize_single_batch(self, batch, is_residue) -> tuple[tf.Tensor]:
         """
         Optimize single batch via tensorflow probability's optimizer LBFGS-minimze.
 
         Args:
-            collocation_batch (tuple(EagerTensor)): one batch of collocation point data
-            one_labelled_batch (tuple(EagerTensor)): one batch of labelled point data
+            batch: one batch of data
+            is_residue: check whether to use residue or labelled loss
 
         Returns:
-            tuple[tf.Tensor]: current_residue_loss, current_labelled_loss
+            current_loss
         """    
             
-        current_labelled_loss = self._compute_labelled_loss(labelled_batch)
-        current_residue_loss = self._compute_residue_loss(collocation_batch)
+        loss = self._compute_residue_loss(batch) if is_residue else self._compute_labelled_loss(batch)
         
         initial_position = tf.concat([tf.reshape(v,[-1]) for v in self.model.trainable_variables], axis = 0)
         
@@ -127,7 +125,7 @@ class pinnOptimizer(nlseGradient):
             self.assign_model_parameters(position)
             
             # Compute the total loss again with updated model parameters
-            total_loss, gradients = self._compute_gradient(collocation_batch, labelled_batch)
+            total_loss, gradients = self._compute_gradient(batch, is_residue)
             
             return total_loss, gradients
         
@@ -138,7 +136,7 @@ class pinnOptimizer(nlseGradient):
         )
         
         self.assign_model_parameters(results.position)
-        return current_residue_loss, current_labelled_loss
+        return loss
 
     def fit(self, epochs):
         """
@@ -152,23 +150,26 @@ class pinnOptimizer(nlseGradient):
             epoch_labelled_loss = 0
             
             collocation_iterator = iter(self.collocation_data)
-            labelled_iterator = cycle(self.labelled_data)
+            labelled_iterator = iter(self.labelled_data)
             print(f"Epoch {epoch+1}/{epochs}")
 
-            progress_bar = tqdm(range(len(self.collocation_data)), desc="Training Batches", unit="batch")
+            collocation_bar = tqdm(range(len(self.collocation_data)), desc="Training Batches", unit="batch")
+            labelled_bar = tqdm(range(len(self.labelled_data)), desc="Labelled Batches", unit="batch")
             
-            for _ in progress_bar:
+            for _ in collocation_bar:
                 collocation_batch = next(collocation_iterator)
+                residue_loss = self.optimize_single_batch(collocation_batch, is_residue = True)
+                collocation_bar.set_description(f"Residue Loss: {residue_loss:.4e}")
+                epoch_residue_loss += residue_loss
+                
+            for _ in labelled_bar:
                 labelled_batch = next(labelled_iterator)
-                
-                batch_residue_loss, batch_labelled_loss = self.optimize_single_batch(collocation_batch, labelled_batch)
-                progress_bar.set_description(f"Total Loss: {batch_residue_loss+batch_labelled_loss:.4f}")
-
-                epoch_residue_loss += batch_residue_loss
-                epoch_labelled_loss += batch_labelled_loss
-                
+                labelled_loss = self.optimize_single_batch(labelled_batch, is_residue = False)
+                labelled_bar.set_description(f"Labelled Loss: {labelled_loss:.4e}")
+                epoch_labelled_loss += labelled_loss
             
-            epoch_residue_loss /= len(self.labelled_data)
+            
+            epoch_residue_loss /= len(self.collocation_data)
             epoch_labelled_loss /= len(self.labelled_data)
             
             print(f'Residue Loss : {epoch_residue_loss:.4f}; Labelled Loss : {epoch_labelled_loss:.4f}')
