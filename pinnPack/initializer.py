13c39b1e3cbab00d5ad87ff14555bd3368b17f52
Adk-Ptr <andikoputrapk@student.ub.ac.id>
2025-03-12 00:11:41 +0000

    Removing dataset batching system, instead convert array into tensor and feed it to optimizer

--- pinnPack/initializer.py
+++ pinnPack/initializer.py
@@ -1,14 +1,15 @@
 import tensorflow as tf 
 import numpy as np
 
-class initData:
+class genData:
     '''
     initData class is used to initialize the collocation points and labelled data using random LHS sampling
     '''
     def __init__(self):
         pass 
-
-    def lhs_sampling(self, n: int, d: int, seed: int = None) -> np.ndarray:
+    
+    @staticmethod
+    def lhs_sampling(n: int, d: int, seed: int = None) -> np.ndarray:
         """
         Latin Hypercube Sampling (LHS) to generate random points
 
@@ -28,7 +29,8 @@ class initData:
         
         return result
     
-    def init_points(self, final: np.ndarray, start: np.ndarray, n: int, seed: int = None) -> np.ndarray:
+    @staticmethod
+    def generate_points(final: np.ndarray, start: np.ndarray, n: int, seed: int = None) -> np.ndarray:
         '''
         Initialize points using Latin Hypercube Sampling
         
@@ -39,86 +41,20 @@ class initData:
             seed (int, optional): Random seed. Defaults to None.
         '''
         d = final.shape[-1] if isinstance(final, np.ndarray) else 1
-        col_points = self.lhs_sampling(n,d,seed)*(final-start) + start
+        col_points = genData.lhs_sampling(n,d,seed)*(final-start) + start
         return col_points
     
-class batchData:
-    '''
-    batchData class is used to store collocation and labelled data and batch for training purposes
-    '''
-    def __init__(self):
-        self.tx_train = {
-            'col_point': None,
-        }
-        self.uv_train = {
-            'col_point': None,
-        }
-    
-    def add_collocation_point(self,tx):
+    @staticmethod
+    def batch_data(tx_data, uv_data) -> dict:
         """
-        Adding set of collocation points to self.tx_train, and
-        expected residual value on self.u_train and self.v_train via tf.zeros_like
-
-        Args:
-            tx (array_like): Inirialized collocation point
+        Batch x_train and y_train (input data) into a dataset
         """        
-        self.tx_train['col_point'] = tx
-        self.uv_train['col_point'] = tf.zeros((tx.shape[0],2))
-    
-    def add_labelled_data(self, tx, u, v, label):
-        """
-        Adding set of labelled data to self.tx_train, self.u_train, and self.v_train
+        
+        dataset = (
+            tf.convert_to_tensor(tx_data, dtype=tf.float32),
+            tf.convert_to_tensor(uv_data, dtype=tf.float32)
+        )
 
-        Args:
-            tx (array_like): Initialized labelled data
-            u (array_like): real part of labelled data
-            v (array_like): imaginary part of labelled data
-        """        
-        self.tx_train[label] = tx
-        self.uv_train[label] = tf.concat([u,v], axis = -1)
-        
-   
-    def get_keys(self)->list:
-        """
-        Get the keys of dataset's dictionary
-
-        Returns:
-            list: List of keys
-        """        
-        return list(self.tx_train.keys())
-        
-    def batch_data(self, batch_size = 100) -> dict:
-        """
-        Batch x_train and y_train (input data)
-
-        Args:
-            batch_size (int or dict[str, int]): Batch size for each key 
-
-        Returns:
-            dictionary of batched data corresponding to each key
-        """        
-        batched_data = {}
-        common_keys = self.get_keys()
-        
-        labelled_keys = list(set(common_keys) - set(['col_point']))
-        tx_labelled = np.concatenate([self.tx_train[key] for key in labelled_keys], axis = 0)
-        uv_labelled = np.concatenate([self.uv_train[key] for key in labelled_keys], axis = 0)
-        
-        collocation_data = tf.data.Dataset.from_tensor_slices((
-            tf.cast(self.tx_train['col_point'], tf.float32), 
-            tf.cast(self.uv_train['col_point'], tf.float32)
-        ))
-
-        labelled_data = tf.data.Dataset.from_tensor_slices((
-            tf.cast(tx_labelled, tf.float32), 
-            tf.cast(uv_labelled, tf.float32)
-        ))
-        
-        collocation_data = collocation_data.batch(batch_size).prefetch(tf.data.AUTOTUNE)
-        labelled_data = labelled_data.batch(batch_size).prefetch(tf.data.AUTOTUNE)
-        batched_data['col_point'] = collocation_data
-        batched_data['labelled_data'] = labelled_data
-        
-        return batched_data
+        return dataset
 
 
