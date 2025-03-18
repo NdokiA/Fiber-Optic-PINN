ae10da6f2f63a406dfcdd45b70daa8a6576816f6
Adk-Ptr <andikoputrapk@student.ub.ac.id>
2025-03-18 06:49:37 +0000

    Rename plotting function in vizPack

--- vizPack/utils.py
+++ vizPack/utils.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt 
 from matplotlib import cm 
 import numpy as np
+import json
+
 from ssfmPack import ssfm
 
 def saveplot(path):
@@ -14,14 +16,22 @@ def plotPulseContour(Z, T, pulse, path, cutoff = -30):
     fig, ax = plt.subplots()
     ax.set_title('Pulse Evolution (dB scale)')
     
-    P = ssfm.getPower(pulse)
-    P[P<1e-100] = 1e-100
-    P = 10*np.log10(P)
-    P[P<cutoff] = cutoff
+    P = ssfm.convertdB(pulse, cutoff)
     
     surf = ax.contourf(Z,T,P, levels = 40)
     ax.set_ylabel('Time [ps]')
     ax.set_xlabel('Distance [km]')
     cbar = fig.colorbar(surf, ax = ax)
     saveplot(path)
-    
+
+def updateJSON(model_name, data):
+    with open("result/report.json", "r") as file:
+        try:
+            loaded = json.load(file)
+        except json.JSONDecodeError:
+            loaded = {}  # Reinitialize as empty dictionary
+
+        loaded[model_name] = data
+    with open("result/report.json", "w") as file:
+        json.dump(loaded, file, indent = 4)
+    print('Data Updated!')
