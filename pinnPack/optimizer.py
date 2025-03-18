56d7a6532391e6d7bc845a92de5689778f109dcd
Adk-Ptr <andikoputrapk@student.ub.ac.id>
2025-03-18 06:45:55 +0000

    Adding ADAM based backpropagation method

--- pinnPack/optimizer.py
+++ pinnPack/optimizer.py
@@ -2,10 +2,10 @@ import tensorflow as tf
 
 import tensorflow_probability as tfp
 from tqdm import tqdm
-from pinnPack.gradient import nlseGradient
-
 import time as Time
-from functools import wraps
+
+from pinnPack.gradient import nlseGradient
+from ssfmPack import ssfm
 
 
 class pinnOptimizer(nlseGradient):
@@ -13,16 +13,17 @@ class pinnOptimizer(nlseGradient):
     Algorithm for processing input and do backpropagation
     '''
     
-    def __init__(self, model, collocation_data, labelled_data ,
+    def __init__(self, model, collocation_data, labelled_data, validation_data,
                  T, L, alpha, beta2, gamma):
         super().__init__(model, T, L, alpha, beta2, gamma)
         self.model = model
         
-        self.ADAM = tf.optimizers.Adam(learning_rate = 1e-3, jit_compile=False)
+        self.ADAM = tf.optimizers.Adam(learning_rate = 5e-4, jit_compile=False)
         self.LBFGS = tfp.optimizer.lbfgs_minimize
         
         self.labelled_data = labelled_data
         self.collocation_data = collocation_data
+        self.validation_data = validation_data
         
         self.alpha = alpha
         self.beta2 = beta2
@@ -33,8 +34,9 @@ class pinnOptimizer(nlseGradient):
         
         self.loss_records = []
         self.time_records = []
+        self.validation_records = []
     
-    def _loss_fn(self, collocation_data, labelled_data) -> tf.Tensor:
+    def _loss_fn(self, collocation_data, labelled_data) -> float:
         
         uv_residue = self.compute_residue(collocation_data[0])
         uv_data = self.compute_labelled_data(labelled_data[0])
@@ -43,6 +45,14 @@ class pinnOptimizer(nlseGradient):
         data_loss = tf.reduce_mean(tf.keras.losses.MSE(uv_data, labelled_data[1]))
                                              
         losses = tf.cast(residual_loss + data_loss, tf.float32)
+        return losses
+    
+    def _validation_fn(self, validation_data) -> float:
+        
+        computed_uv = self.compute_residue(validation_data[0])
+        pulse = tf.reduce_sum(tf.square(computed_uv), axis = 1) 
+        
+        losses = tf.reduce_mean(tf.keras.losses.MSE(pulse, validation_data[1]))
         return losses
 
     def _gradient(self, collocation_data, labelled_data) -> tuple:        
@@ -95,7 +105,7 @@ class pinnOptimizer(nlseGradient):
             value_and_gradients_function = value_and_gradients_function,
             initial_position = initial_position,
             tolerance = 1e-5,
-            max_iterations = 20,
+            max_iterations = 50,
         )
         
         self.assign_model_parameters(results.position)
@@ -118,10 +128,13 @@ class pinnOptimizer(nlseGradient):
             loss = self.optimize_adam(self.collocation_data, self.labelled_data)
             stop = Time.time()
             
-            adam_bar.set_postfix(loss=f"{loss:.4e}")
+            val_loss = self._validation_fn(self.validation_data)
+            adam_bar.set_postfix({"Training Loss": f"{loss:.4e}", "Validation Loss": f"{val_loss:.4e}"})
             total_time += (stop-start)
+            
             self.time_records.append(total_time)            
             self.loss_records.append(loss)
+            self.validation_records.append(val_loss)
         
         print('Processing using L-BFGS Optimalization strategies')
         for epoch in bfgs_bar:
@@ -129,7 +142,10 @@ class pinnOptimizer(nlseGradient):
             loss = self.optimize_lbfgs(self.collocation_data, self.labelled_data)
             stop = Time.time()
             
-            bfgs_bar.set_postfix(loss=f"{loss:.4e}")
+            val_loss = self._validation_fn(self.validation_data)
+            bfgs_bar.set_postfix({"Training Loss": f"{loss:.4e}", "Validation Loss": f"{val_loss:.4e}"})
             total_time += (stop-start)
-            self.time_records.append(total_time)  
+            
+            self.time_records.append(total_time)            
             self.loss_records.append(loss)
+            self.validation_records.append(val_loss)
